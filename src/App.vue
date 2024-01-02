914a2c5b15ca3917e956a5e8bdc1ac0bda50f47e
kaleb96 <yes505304@gmail.com>
2024-01-02 17:52:52 +0900

    Apply pagination and Vuetify card
    - [x] apply pagination
    - [x] apply vuetfiy v-card

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <v-app>
-    <v-app-bar>Rodden Potatoes
-      <v-card>
+    <v-app-bar>
+        <v-container>
+          <v-card>
+            <div>Rodden Potatoes</div>
+          </v-card>
+        </v-container>
+
+      <v-card class="search">
               <input v-model.trim="search" type="text" placeholder="Search Movies...">
               <v-btn @click="fetchMovies">Search</v-btn>
       </v-card>
     </v-app-bar>
-    <v-navigation-drawer>
-        <v-btn @click="getMovieList">Various Movies</v-btn>
 
+    <v-navigation-drawer>
+        <div class="movies" @click="getMovieList">Various Movies</div>
     </v-navigation-drawer>
+
     <v-main>
         <v-container>
             <v-card v-if="searchResult.Title">
@@ -22,21 +29,65 @@
                 <p v-for="(rating, idx) in searchResult.Ratings" :key="idx">
                   {{ rating.Source }} | {{ rating.Value }} <br>
                 </p>
-            </v-card>
-            <v-card v-if="allMovieLists" >
-              <p v-for="movieInfo in allMovieLists" :key="Poster">
+              </v-card>
+        </v-container>
+
+
+
+        <v-container>
+          <v-row align="center" justify="center">
+            <v-col
+              v-for="movieInfo in tmpLists"
+              :key="movieInfo.Poster"
+              cols="auto"
+            >
+              <v-card
+                class="mx-auto"
+                max-width="344"
+                :variant="movieInfo"
+              >
+                <v-card-item>
+                  <div>
+                    <img :src="movieInfo.Poster">
+                    <div class="text-h4">{{ movieInfo.Title }}</div>
+                  </div>
+                </v-card-item>
+              </v-card>
+            </v-col>
+          </v-row>
+
+          <!-- <v-card
+            :title="movieInfo.Title"
+            subtitle="Subtitle"
+            variant="tonal"
+            v-for="movieInfo in tmpLists" :key="movieInfo.Poster"
+            >
+            <img :src="movieInfo.Poster">
+          </v-card> -->
+
+
+
+        </v-container>
+          <!-- <v-card class=text-center>
+
+              <p v-for="movieInfo in tmpLists" :key="movieInfo.Poster" >
                   <img :src="movieInfo.Poster">
                   <p>{{ movieInfo.Title }}</p>
               </p>
-            </v-card>
-        </v-container>
-    </v-main>
-    <v-footer></v-footer>
-  </v-app>
+            </v-card> -->
+
+            <v-btn v-for="page in totalPage" :key="page" @click="checkPage(page)">{{ page }}</v-btn>
+            <v-btn>totalItems : {{ totalItems }}</v-btn>
+
+      </v-main>
+
+      <v-footer></v-footer>
+
+ </v-app>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed} from 'vue';
 import axios from 'axios';
 
 // state
@@ -47,6 +98,32 @@ const getMovieIds = ref([]);
 const imdbList = ref([]);
 const allUrlList = ref([]);
 const allMovieLists = ref([]);
+const items = 10;
+const currentPage = ref(1);
+const totalItems = computed(() => {
+  return allMovieLists.value.length;
+});
+
+const totalPage = computed(() => {
+  return Math.ceil(allMovieLists.value.length/items);
+});
+
+
+const checkPage = (page) => {
+  currentPage.value = page;
+}
+
+
+/* watch and update allMovieLists each pages */
+const tmpLists = computed(() => {
+
+  const start = (currentPage.value - 1) * items;
+  const end = start + items;
+  // console.log('tmpLists' + start + ' | ' + end);
+  return allMovieLists.value.slice(start, end)
+})
+
+
 // functions
 
 /** Movie Search API */
@@ -147,14 +224,25 @@ const getAllMovieList = async() => {
   } catch (error){}
 }
 
+/* Listing All movies Data */
 const fechData = async(url) => {
 
   try {
     const response = await axios.get(url);
-    if(response.data.Poster != 'N/A') {
+    if(response.data.Poster != 'N/A' && response.data.Poster != '' && response.data.Poster != null) {
       allMovieLists.value.push(response.data);
     }
-    // console.log('AllMovieLists = ' + JSON.stringify(allMovieLists.value))
+    // console.log('AllMovieLists = ' + JSON.stringify(allMovieLists.value.length))
   } catch (error) {}
 }
 </script>
+
+<style scoped>
+  .text-center {
+    display: flex;
+    flex-wrap: wrap;
+
+  }
+
+
+</style>
